4db2ed88157fd50b655f593c864d7d969dd7eaf3
Qhy23 <18137798371@163.com>
2025-03-28 22:34:41 +0800

    更新班级日志

--- ClassDiary/book/toc.js
+++ ClassDiary/book/toc.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="introduction.html"><strong aria-hidden="true">1.</strong> 主页</a></li><li class="chapter-item expanded affix "><li class="part-title">正文</li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="introduction.html"><strong aria-hidden="true">1.</strong> 主页</a></li><li class="chapter-item expanded affix "><li class="part-title">正文</li><li class="chapter-item expanded "><a href="班级日志/202503.html"><strong aria-hidden="true">2.</strong> 2025年3月</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString().split("#")[0];
         if (current_page.endsWith("/")) {
