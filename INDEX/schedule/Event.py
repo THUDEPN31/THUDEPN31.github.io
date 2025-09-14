3993e3d3b77e542f11ccea381c8a5135ea13b856
Qhy23 <18137798371@163.com>
2025-09-14 17:30:06 +0800

    2025-2026秋季学期

--- INDEX/schedule/Event.py
+++ INDEX/schedule/Event.py
@@ -7,19 +7,19 @@ from datetime import datetime, timedelta, time
 '''
 
 # 本学期开学日期
-DATE = "2025/6/23"
+DATE = "2025/9/15"
 # 本学期周数
-WEEK = 12
+WEEK = 23
 
 '''
 以下代码一般不需修改
 '''
 
-# 导入excel数据与预处理
-excel_file = 'INDEX/schedule/Event.xlsx'
-df = pd.read_excel(excel_file)
-df['开始日期'] = df['开始日期'].dt.strftime('%Y/%m/%d')
-df['结束日期'] = df['结束日期'].dt.strftime('%Y/%m/%d')
+# 导入CSV数据与预处理
+csv_file = 'Event.csv'
+df = pd.read_csv(csv_file)
+df['开始日期'] = pd.to_datetime(df['开始日期'], errors='coerce').dt.strftime('%Y/%m/%d')
+df['结束日期'] = pd.to_datetime(df['结束日期'], errors='coerce').dt.strftime('%Y/%m/%d')
 events_list = df.to_dict(orient='records')
 
 # 进行日期表示法与周次星期表示法的双向处理
@@ -42,20 +42,15 @@ def WeekDay2Date(week, day):
     date2 = date1 + timedelta(days=delta)
     return date2.strftime(date_format)
 
-def timeserializer(obj):
-    if isinstance(obj, time):
-        return obj.strftime('%H:%M')
-
 i = 1
 for event in events_list:
     i += 1
     if "具体时间待定" in str(event["描述"]):
-        event["开始时间"] = time(23,59)
-        event["结束时间"] = time(23,59)
+        event["开始时间"] = time(23,59).strftime('%H:%M')
+        event["结束时间"] = time(23,59).strftime('%H:%M')
     if pd.isna(event["开始时间"]):
         print(f"开始信息无法自动补全，错误行数{i}")
     else:
-        event["开始时间"] = timeserializer(event["开始时间"])
         if pd.isna(event["开始日期"]):
             if pd.isna(event["开始周次"]) or pd.isna(event["开始星期"]):
                 print(f"开始信息无法自动补全，错误行数{i}")
@@ -68,7 +63,6 @@ for event in events_list:
     if pd.isna(event["结束时间"]):
         print(f"结束信息无法自动补全，错误行数{i}")
     else:
-        event["结束时间"] = timeserializer(event["结束时间"])
         if pd.isna(event["结束日期"]):
             if pd.isna(event["结束周次"]) or pd.isna(event["结束星期"]):
                 event["结束日期"] = event["开始日期"]
@@ -86,7 +80,7 @@ for event in events_list:
         event["地点"] = ""
 
 # 转换为所需的JSON格式
-formatted_events = [{"startdate": event["开始日期"], 
+formatted_events = [{"startdate": event["开始日期"],
                      "starttime": event["开始时间"],
                      "startweek": event["开始周次"],
                      "startday": event["开始星期"],
@@ -101,5 +95,5 @@ formatted_events = [{"startdate": event["开始日期"],
                      } for event in events_list]
 
 # 将列表写入JSON文件
-with open('INDEX/schedule/Event.json', 'w', encoding='utf-8') as f:
+with open('Event.json', 'w', encoding='utf-8') as f:
     json.dump(formatted_events, f, ensure_ascii=False, indent=4)
